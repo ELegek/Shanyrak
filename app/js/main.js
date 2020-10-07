c5d34cf5060c10100162dc2cc0a209016851e7b0
Evgeny Lezhepekov <evgeny.legek@yandex.ru>
2020-10-07 21:47:40 +0300

    Burger menu

--- app/js/main.js
+++ app/js/main.js
@@ -1,14 +1,24 @@
-$(function(){
+$(function () {
 
-$('.slider').slick({
-    prevArrow: '<div class="btn-container"><button class="slider-btn slider-btn__left"><svg width="11" height="22" viewBox="0 0 11 22" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M9.32721 1L1 11L9.32721 21" stroke="black" stroke-width="2" stroke-miterlimit="10" stroke-linejoin="round"/></svg></button></div>',
 
-    nextArrow:'<div class="btn-container"><button class="slider-btn slider-btn__right"><svg width="11" height="22" viewBox="0 0 11 22" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1.67279 1L10 11L1.67279 21" stroke="black" stroke-width="2" stroke-miterlimit="10" stroke-linejoin="round"/></svg></button></div>',
-    dots: true,
-    fade: true,
-    autoplay: true,
-    autoplaySpeed: 3000
-});
+    // Slider 
+    $('.slider').slick({
+        prevArrow: '<div class="btn-container"><button class="slider-btn slider-btn__left"><svg width="11" height="22" viewBox="0 0 11 22" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M9.32721 1L1 11L9.32721 21" stroke="black" stroke-width="2" stroke-miterlimit="10" stroke-linejoin="round"/></svg></button></div>',
+
+        nextArrow: '<div class="btn-container"><button class="slider-btn slider-btn__right"><svg width="11" height="22" viewBox="0 0 11 22" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1.67279 1L10 11L1.67279 21" stroke="black" stroke-width="2" stroke-miterlimit="10" stroke-linejoin="round"/></svg></button></div>',
+        dots: true,
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 3000
+    });
+
+
+    //Menu burger
+    document.querySelector('.header__burger').onclick = function() {
+        document.querySelector('.menu__btn').classList.toggle('active');
+        document.querySelector('.header__menu').classList.toggle('active');
+        
+    };
 
 
 });
